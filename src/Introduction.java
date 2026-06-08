a4b566f41224641dcca31a18dcacf6bbb8ccb3a6
WadhaAlgarni <wadhaalgrne68@gmail.com>
2026-06-08 23:04:05 +0300

    First Java lesson - Introduction

---
+++ src/Introduction.java
@@ -0,0 +1,22 @@
+
+public class Introduction {
+    public static void main(String[] args) {
+        //psvm shortcut to generate:public static void main(String[] args)
+
+        //1- Type of comments
+        //Single-line comment
+        /*
+        * Multi-line comment
+        */
+
+        //2- Printing to Screen
+        //sout shortcut to generate:System.out.println
+        System.out.println("Hello Java!");
+        System.out.print("Wadha");
+
+    }
+
+}
+//Package
+///class
+////Method
